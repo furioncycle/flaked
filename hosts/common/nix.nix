9d2913c8116c1ac35e21aaa21f85433452b46718
furioncycle <ttecho2021@gmail.com>
2024-11-06 20:35:20 -0800

    updated lock and fixed failures

--- hosts/common/nix.nix
+++ hosts/common/nix.nix
@@ -22,7 +22,7 @@ with lib;
     settings = {
       accept-flake-config = true;
       auto-optimise-store = mkDefault true;
-      experimental-features = [ "nix-command" "flakes" "repl-flake" ];
+      experimental-features = [ "nix-command" "flakes" ];
       # show more log lines for failed builds
       log-lines = 30;
       # Free up to 10GiB whenever there is less than 5GB left.
@@ -34,7 +34,7 @@ with lib;
       warn-dirty = false;
     };
 
-    package = pkgs.nixFlakes;
+    package = pkgs.nixVersions.stable;
     registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
     nixPath = [ "nixpkgs=${inputs.nixpkgs.outPath}" ];
   };
