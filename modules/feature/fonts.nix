fd111b46ad728387504f0040084bd2efc22c0ccb
furioncycle <ttecho2021@gmail.com>
2024-11-30 14:24:31 -0800

    update flake & removed unnecessary fonts

--- modules/feature/fonts.nix
+++ modules/feature/fonts.nix
@@ -24,49 +24,23 @@ with lib;
       fontDir.enable = true;
 
       packages = with pkgs; [
-        caladea
-        cantarell-fonts
-        carlito
-        courier-prime
-        dejavu_fonts
-        font-awesome
-        gelasio
-        liberation_ttf
-        material-design-icons
-        merriweather
-        noto-fonts
-        noto-fonts-emoji
-        open-sans
-        roboto
-        ubuntu_font_family
-        weather-icons
-
-        # nerdfonts
-        (nerdfonts.override {
-          fonts = [
-            "DroidSansMono"
-            "Hack"
-            "JetBrainsMono"
-            "Noto"
-          ];
-        })
+        atkinson-hyperlegible
+        atkinson-monolegible
       ];
 
       # user defined fonts
-      # the reason there's Noto Color Emoji everywhere is to override DejaVu's
-      # B&W emojis that would sometimes show instead of some Color emojis
       fontconfig = mkIf graphics.enable {
         enable = mkDefault true;
         antialias = mkDefault true;
         cache32Bit = mkDefault true;
         hinting.enable = mkDefault true;
         hinting.autohint = mkDefault true;
-        defaultFonts = {
-          serif = [ "Noto Serif NF" "Noto Color Emoji" ];
-          sansSerif = [ "Noto Sans NF" "Noto Color Emoji" ];
-          monospace = [ "Hack Nerd Font" "Noto Color Emoji" ];
-          emoji = [ "Noto Color Emoji" ];
-        };
+        # defaultFonts = {
+        # serif = [ "Noto Serif Nerd Font" "Noto Color Emoji" ];
+        # sansSerif = [ "Noto Sans Nerd Font" "Noto Color Emoji" ];
+        # monospace = [ "JetBrainsMono Nerd Font" "Noto Color Emoji" ];
+        # emoji = [ "Noto Color Emoji" ];
+        # };
       };
     };
   };
